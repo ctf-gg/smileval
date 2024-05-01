78f87b0a8554a18ff09910f71a343a3ff2ae87d6
smashmaster0045 <smashmaster@amateurs.team>
2024-05-01 07:34:59 +0000

    basic stuffs

---
+++ models/base.py
@@ -0,0 +1,58 @@
+
+class ChatMessage:
+    def __init__(self, content: str, role: str = "user"):
+        self.content = content
+        self.role = role
+
+    def as_dict(self):
+        return {
+            "content": content,
+            "role": role
+        }
+    
+    # a bit unpythonic but there if I need it
+    def clone(self):
+        return ChatMessage(self.content, self.role)
+
+    # why do these exist? I forgot.
+    def is_system(self):
+        return self.role == "system"
+
+    def is_user(self):
+        return self.role == "user"
+
+    def is_assistant(self):
+        return self.role == "assistant"
+
+    def unsystem_prompt(self):
+        '''
+        Some models like certain Llama models may not allow a system prompt. This is a bad hack to fake a system prompt as a user message but it is recommended to use the utility message to rewrite a message chain.
+        '''
+        assert self.role == "system"
+        self.content = f"# System\n{self.content}"
+    @staticmethod
+    def from_dict(message):
+        return ChatMessage(message["content"], message["role"] if message["role"] else "user")
+
+# TODO: add tests
+def unsystem_prompt_chain(messages: list[ChatMessage]):
+    '''
+    Models like Mixtral don't have a system prompt but a friend made up this hack by prepending the system message 2 line breaks before the first user message that works suprisingly well.
+    '''
+    system_messages_merge = ""
+    # why would you use more than one system message
+    for system_message in filter(lambda m: m.role == "system", messages):
+        system_messages_merge += system_message_merge + system_message.content + "\n"
+    other_messages = list(filter(lambda m: m.role != "system", messages))
+    assert len(other_messages) > 0, "Cannot unsystem message a message chain with only system messages."
+    assert other_messages[0].is_user(), "First non-system message must be from user"
+    other_messages[0].content = system_messages_merge + "\n" + other_messages[0].content
+    return other_messages
+
+class ChatCompletionModel:
+    def __init__(self, name: str):
+        self.name = name
+
+class EmbeddingModel:
+    def __init__(self, name: str):
+        self.name = name
