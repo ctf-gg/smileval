b3bfee4754368495ae5778e7360cabffee39d146
smashmaster0045 <smashmaster@amateurs.team>
2024-05-01 22:27:40 +0000

    guess litellm bulk embed format

--- smileval/models/litellm.py
+++ smileval/models/litellm.py
@@ -1,8 +1,18 @@
-from .base import ChatCompletionModel, ChatMessage, ChatCompletionOptions, default_options
+from .base import ChatCompletionModel, ChatMessage, ChatCompletionOptions, EmbeddingModel, default_options
 
-from litellm import acompletion
+from litellm import aembedding
 
 class LiteLLMChatCompletionModel(ChatCompletionModel):
     async def chat_complete(self, messages: list[ChatMessage], options: ChatCompletionOptions = default_options) -> ChatMessage:
         completions = await acompletion(model = self.name, messages = ChatMessage.to_api_format(messages))
         return ChatMessage.from_dict(completions['choices'][0]['message'])
+
+class LiteLLMEmbeddingModel(EmbeddingModel):
+    async def embed(self, messages: str | list[str]) -> list[list[float]]:
+        '''
+        reference api format guessed from https://github.com/BerriAI/litellm/blob/main/litellm/tests/test_caching.py
+        '''
+        embedding_objects = await aembedding(self.name, input = messages)
+        return [
+            embedding_object["embedding"] for embedding_object in embedding_objects.data
+        ]
--- smileval/models/ollama.py
+++ smileval/models/ollama.py
@@ -6,7 +6,7 @@ from ollama import AsyncClient
 import os
 import asyncio
 
-class OllamaChatCompletionModel(ChatCompletionModel):
+class OllamaEmbeddingModel(ChatCompletionModel):
     def __init__(self, name: str, host: str | None = None):
         super().__init__(name)
         opts = {}
@@ -20,7 +20,7 @@ class OllamaChatCompletionModel(ChatCompletionModel):
         completion = await self.client.chat(model = self.name, messages = ChatMessage.to_api_format(messages))
         return ChatMessage.from_dict(completion['message'])
 
-class OllamaChatCompletionModel(EmbeddingModel):
+class OllamaEmbeddingModel(EmbeddingModel):
     def __init__(self, name: str, host: str | None = None):
         super().__init__(name)
         opts = {}
@@ -30,7 +30,7 @@ class OllamaChatCompletionModel(EmbeddingModel):
             opts["host"] = host
         self.client = AsyncClient(**opts)
 
-    # are batch embeddings a thing?
+    # are batch embeddings a thing? will be literally serial for now
     async def embed_one(self, message: str) -> list[float]:
         embedding = await self.client.embeddings(self.name, message)
         return embedding
