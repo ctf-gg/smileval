1a7618eacdd827259e4eef5438e842d27842e6df
smashmaster0045 <smashmaster@amateurs.team>
2024-05-02 21:46:01 +0000

    fix significant randomization accuracy loss

--- smileval/loaders/mcq.py
+++ smileval/loaders/mcq.py
@@ -21,6 +21,7 @@ MCQ_EXAMPLE_CORRECT_INDEX = 1
 
 class MCQQuestionAskExperiment(Experiment):
     def __init__(self, question: str, answer_choices: list[str], correct_answers: list[str] = [], formatting = default_formatting, points: int = 1, use_shuffle = False, use_example = False, use_strict = False):
+        super().__init__()
         self.question = question
         self.answer_choices = answer_choices
         self.correct_answers = correct_answers
@@ -29,30 +30,33 @@ class MCQQuestionAskExperiment(Experiment):
         self.correct_indexes = [
             self.answer_choices.index(answer) for answer in correct_answers
         ]
+        self.formatting = formatting
         self.metadata = ExperimentMetadata(self.gen_id(), points)
         # give one sample interaction chain to help weaker LLMs
         self.use_example = use_example
         self.shuffle = use_shuffle
         self.max_points = points
-        self.formatting = formatting
         self.use_strict = use_strict
 
     def gen_id(self):
         # question is not shuffled yet so we can id it
         self.backup_answer_choices = self.answer_choices[:]
-        self.answer_choices = sorted(self.answer_choices)
+        self.answer_choices = sorted(self.answer_choices[:])
         text = self.format_question(default_formatting)
         q_id = sha256(text)[:6]
-        self.answer_choices = self.backup_answer_choices
+        # print("backup",self.backup_answer_choices, self.answer_choices)
+        self.answer_choices = self.backup_answer_choices[:]
         return q_id
 
     def format_question_example(self, formatting_override = None) -> str:
         qa_format = formatting_override if formatting_override else self.formatting
         return qa_format["question_answer_format"].format(MCQ_EXAMPLE_QUESTION,mcq_templates.format_choices(MCQ_EXAMPLE_ANSWERS, qa_format["presentation_selection_type"]["choices"], qa_format["sep"]))
 
-    def format_question(self, formatting_override = None) -> str:
+    def format_question(self, answer_choices = None, formatting_override = None) -> str:
+        if not answer_choices:
+            answer_choices = self.answer_choices
         qa_format = formatting_override if formatting_override else self.formatting
-        return qa_format["question_answer_format"].format(self.question,mcq_templates.format_choices(self.answer_choices, qa_format["presentation_selection_type"]["choices"], qa_format["sep"]))
+        return qa_format["question_answer_format"].format(self.question,mcq_templates.format_choices(answer_choices, qa_format["presentation_selection_type"]["choices"], qa_format["sep"]))
 
     async def execute(self, context: ExperimentContext) -> ExperimentOutcome:
         outcome = ExperimentOutcome(self.get_metadata(), context)
@@ -62,7 +66,7 @@ class MCQQuestionAskExperiment(Experiment):
         if self.shuffle:
             random.shuffle(answer_choices)
 
-        prompt = self.format_question()
+        prompt = self.format_question(answer_choices)
 
         system_prompt = default_formatting["presentation_selection_type"]["system"]
 
@@ -88,7 +92,7 @@ class MCQQuestionAskExperiment(Experiment):
         if not self.use_strict:
             # reverse
             correct_symbols = [self.formatting["sep"].format(self.formatting["presentation_selection_type"]["choices"][answer_choices.index(correct_answer)], "").strip() for correct_answer in self.correct_answers]
-            # print("Correct symbols",correct_symbols)
+            # print("Correct symbols", correct_symbols, self.correct_answers, answer_choices.index(self.correct_answers[0]))
             for correct_symbol in correct_symbols:
                 if correct_symbol in answer:
                     outcome.set_score_off_bool(True)
